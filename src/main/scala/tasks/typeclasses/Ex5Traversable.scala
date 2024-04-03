6e2ef92e1aa96ef959130b062725d445ca377c41
Claudio Verazza <claudio.verazza@studio.unibo.it>
2024-04-03 17:55:22 +0200

    Solution to ex 5

--- src/main/scala/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,7 @@
 package u04lab
 import u03.Sequences.* 
 import Sequence.*
+import u03.Optionals.Optional
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -17,10 +18,45 @@ import Sequence.*
 
 object Ex5Traversable:
 
-  def log[A](a: A): Unit = println("The next element is: "+a)
+  trait Traversable[T[_]]:
+    def log[A](t: T[A]): Unit
+    def logAll[A](t: T[A]): Unit
+
+  given Traversable[Optional] with
+    override def logAll[A](t: Optional[A]): Unit = log(t)
+
+    override def log[A](t: Optional[A]): Unit = t match
+      case Optional.Just(e) => println(e)
+      case _ => println("Empty")
+
+  given Traversable[Sequence] with
+
+    def logAll[A](t: Sequence[A]): Unit = t match
+      case Cons(head, tail) => log(t); logAll(tail)
+      case _ => ()
+    def log[A](t: Sequence[A]): Unit = t match
+      case Cons(h, _) => println("The next element is: " + h)
+      case _ => println("Empty sequence")
+  
+  /*def log[A](a: A): Unit = println("The next element is: "+a)
 
   def logAll[A](seq: Sequence[A]): Unit = seq match
     case Cons(h, t) => log(h); logAll(t)
-    case _ => ()
+    case _ => ()*/
 
-  
+  object LoggableOptional extends Traversable[Optional]:
+
+    def logAll[A](t: Optional[A]): Unit = summon[Traversable[Optional]].logAll(t)
+    def log[A](t: Optional[A]): Unit = summon[Traversable[Optional]].log(t)
+
+  object LoggableSequence extends Traversable[Sequence]:
+
+    def logAll[A](t: Sequence[A]): Unit = summon[Traversable[Sequence]].logAll(t)
+
+    def log[A](t: Sequence[A]): Unit = summon[Traversable[Sequence]].log(t)
+    
+
+@main def tryTraversables() =
+  import u04lab.Ex5Traversable.*
+  LoggableOptional.logAll(Optional.Empty())
+  LoggableSequence.logAll(Cons(3, Cons(2, Cons(1, Nil()))))
